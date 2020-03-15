7ed31625be98e20e3438f61fa7e2bd92f12cfe65
syler <hileo77@gmail.com>
2020-03-15 22:54:07 +0100

    [server] main.ts add helment and cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
+import helmet from 'helmet';
 import { LoggingInterceptor } from './logger/logging.interceptor';
 import { HttpExceptionFilter } from './logger/exeption.logger.filter';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors({ origin: process.env.CORS_ORIGIN });
+  app.enableCors({ origin: process.env.CORS_ORIGIN, credentials: true });
+  app.use(cookieParser());
 
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
