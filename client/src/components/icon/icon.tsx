147bcb807e779e169ae53dbd5dd8ca341c53aac3
syler <hileo77@gmail.com>
2020-04-18 09:43:20 +0200

    [client] update icon component.

--- client/src/components/icon/icon.tsx
+++ client/src/components/icon/icon.tsx
@@ -1,25 +1,35 @@
 import { h, FunctionComponent } from 'preact';
 import * as github from '@fortawesome/free-brands-svg-icons/faGithub';
+import * as google from '@fortawesome/free-brands-svg-icons/faGoogle';
 import * as home from '@fortawesome/free-solid-svg-icons/faHome';
 import * as logout from '@fortawesome/free-solid-svg-icons/faSignOutAlt';
+import * as list from '@fortawesome/free-solid-svg-icons/faList';
+import * as edit from '@fortawesome/free-solid-svg-icons/faEdit';
 import './icon.sass';
 
 const icons = {
   github,
   home,
-  logout
+  logout,
+  list,
+  edit,
+  google,
 };
 
 export type IconKeys = keyof typeof icons;
 
-const IconComponent: FunctionComponent<{ name: IconKeys; flipX?: boolean }> = props => {
+const IconComponent: FunctionComponent<{ name: IconKeys; flipX?: boolean }> = (props) => {
   const icon = icons[props.name];
   if (!icon) {
     return <div class="icon"> </div>;
   }
 
   return (
-    <svg class={`icon${props.flipX ? ' icon-flipX' : ''}`} role="img" viewBox="0 0 512 512">
+    <svg
+      class={`icon${props.flipX ? ' icon-flipX' : ''}`}
+      role="img"
+      viewBox={`0 0 ${icon.width} ${icon.height}`}
+    >
       <path fill="currentColor" d={icon.svgPathData}></path>
     </svg>
   );
