d7921ac61664a715ec5473e2697fd4cffc2931de
syler <hileo77@gmail.com>
2020-07-28 18:43:41 +0200

    [server] remove movie and list

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -12,9 +12,6 @@ import { UserRole } from 'src/shared/utils.auth';
 import { POST } from 'src/shared/api.POST';
 import { SuccessResponse } from 'src/shared/api.response.success';
 import { User } from 'src/entities/user.entity';
-import { MovieSuggestion } from 'src/entities/movieSuggestion.entity';
-import { GET } from 'src/shared/api.GET';
-import { List } from 'src/entities/list.entity';
 
 @UseGuards(AuthGuard)
 @Controller('user')
@@ -40,44 +37,5 @@ export class UserController {
     await user.save();
 
     return new SuccessResponse();
-  }
-
-  @Get('/list')
-  async getUsersLists(@Req() req: AuthRequest): Promise<GET['user/list']['response']> {
-    const username: string = req.query.username || req?.token?.username;
-
-    if (!username) throw new BadRequestException();
-
-    const user = await User.findOne({ where: { username } });
-
-    if (user) {
-      return await List.find({
-        where: { createdBy: user },
-        relations: ['movies'],
-        select: ['description', 'name']
-      });
-    }
-
-    throw new BadRequestException(`User ${username} Not Found.`);
-  }
-
-  @Get('/suggestions')
-  async getSuggestions(@Req() req: AuthRequest): Promise<GET['user/suggestions']['response']> {
-    const username = req.query.username || req?.token?.username;
-
-    if (!username) throw new BadRequestException();
-
-    const user = await User.findOne({ where: { username } });
-
-    if (user) {
-      const suggestions = await MovieSuggestion.find({
-        where: { suggestedBy: user },
-        relations: ['movie', 'suggestion']
-      });
-      if (suggestions) {
-        return suggestions;
-      }
-    }
-    throw new BadRequestException(`User ${username} Not Found.`);
   }
 }
