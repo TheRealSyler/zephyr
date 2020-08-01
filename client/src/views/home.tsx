9f48de86349b06f07e9247775afec9b80b691c0c
syler <hileo77@gmail.com>
2020-08-01 19:11:45 +0200

    [client] show articles on home view

--- client/src/views/home.tsx
+++ client/src/views/home.tsx
@@ -1,12 +1,25 @@
 import { h, FunctionComponent } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+import { GET } from '../api';
+import { Article } from '../shared/api.interfaces';
+import ShowArticles from '../components/showArticles';
 interface HomeViewProps {}
 
 const HomeView: FunctionComponent<HomeViewProps> = () => {
-  return (
-    <div>
-      <div style="margin: 100px"></div>
-    </div>
-  );
+  const [data, setData] = useState<Article[] | null>(null);
+  const getData = async () => {
+    const body = (await GET('article', { results: '20' })).body as Article[];
+
+    if (data === null) {
+      setData(body);
+    }
+  };
+  useEffect(() => {
+    if (!data) {
+      getData();
+    }
+  });
+  return <ShowArticles articles={data} />;
 };
 
 export default HomeView;
