33be8944dbc03ac02a6e01365b3bd84ffa0d2838
syler <hileo77@gmail.com>
2020-08-01 19:09:18 +0200

    [client] create showArticle component.

---
+++ client/src/components/showArticles.tsx
@@ -0,0 +1,32 @@
+import { h, FunctionComponent } from 'preact';
+import { Article } from '../shared/api.interfaces';
+import ArticleCard from './articleCard/articleCard';
+
+interface ShowArticlesProps {
+  articles: Article[] | null;
+}
+
+const ShowArticles: FunctionComponent<ShowArticlesProps> = (props) => {
+  const { articles } = props;
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
+        gap: 40,
+        padding: '4rem',
+      }}
+    >
+      {articles?.map((article) => (
+        <ArticleCard
+          description={article.description}
+          url={`${article.createdBy}/${article.name}`}
+          title={article.title}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ShowArticles;
